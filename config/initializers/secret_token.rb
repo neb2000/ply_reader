4fb1a9cb1e450474f9f456807746c9b847d7cda3
Shuo Chen <nebaries@gmail.com>
2014-12-07 14:11:49 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlyReader::Application.config.secret_token = '46c9bb6d77759203c5aaec7f0186ef52e5cdfb1b72423ee360327088fb39766f9f3fa9b3a2ba700b85a1fa9c435e9b516403c72295da820cda0ca6de512effcf'
